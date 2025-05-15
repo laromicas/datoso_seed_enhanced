c6ea84d38ba1179ad6668aa25460a0df5b5f24ec
laromicas <laromicas@hotmail.com>
2025-05-14 20:10:49 -0500

    - Github actions

--- src/datoso_seed_enhanced/fetch.py
+++ src/datoso_seed_enhanced/fetch.py
@@ -10,5 +10,5 @@ def fetch() -> None:
     folder_helper = Folders(seed=__prefix__)
     folder_helper.clean_dats()
     folder_helper.create_all()
-    archive = Archive(dat_folder='DAT', item='Enhanced-ROMs-2025')
+    archive = Archive(dat_folder='DATs', item='Enhanced-ROMs-2025')
     fetch_helper(archive, folder_helper, __prefix__)
